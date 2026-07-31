bdc68beef068b43ffa46d417d0511631ec5e5a27
simplex17 <258639843+simplex17@users.noreply.github.com>
2026-07-31 18:48:49 +0530

    Adds calculator logic and styling

--- script.js
+++ script.js
@@ -1,39 +1,124 @@
-let a, b, operator;
+let [firstOperand, secondOperand, operator] = ["", "", ""];
+let postOp = false;
+const display = document.querySelector(".calculator__display");
+const buttons = document.querySelectorAll(".calculator__button, .calculator__button-lg");
+let decimalButton;
 
-const add = function (a, b) {
-    return a + b;
+const reset = () => {
+    firstOperand = secondOperand = operator = "";
+    postOp = false;
+    decimalButton.disabled = false;
+    updateDisplay("");
 }
 
-const subtract = function (a, b) {
-    return a - b;
-}
+const backspace = () => {
+    if (postOp) return;
 
-const multiply = function (a, b) {
-    return a * b;
-}
-
-const divide = function (a, b) {
-    return a / b;
-}
-
-const operate = function (a, b, operator) {
-    switch (operator) {
-        case "+": {
-            add(a, b);
-            break;
-        }
-        case "-": {
-            subtract(a, b);
-            break;
-        }
-        case "*": {
-            multiply(a, b);
-            break;
-        }
-        case "/": {
-            divide(a, b);
-            break;
-        }
+    if (operator) {
+        secondOperand = secondOperand.slice(0, secondOperand.length - 1);
+        updateDisplay(secondOperand);
+    } else {
+        firstOperand = firstOperand.slice(0, firstOperand.length - 1);
+        updateDisplay(firstOperand);
     }
 }
 
+const add = (x, y) => x + y;
+const subtract = (x, y) => x - y;
+const multiply = (x, y) => x * y;
+const divide = (x, y) => x / y;
+const operate = () => {
+    if (!(firstOperand && secondOperand && operator)) return;
+
+    firstOperand = parseFloat(firstOperand);
+    secondOperand = parseFloat(secondOperand);
+    let result;
+
+    switch (operator) {
+        case "+":
+            result = add(firstOperand, secondOperand);
+            break;
+
+        case "-":
+            result = subtract(firstOperand, secondOperand);
+            break;
+
+        case "*":
+            result = multiply(firstOperand, secondOperand);
+            break;
+
+        case "/":
+            if (secondOperand === 0) {
+                reset();
+                updateDisplay("Can't divide by zero");
+                return;
+            }
+            result = divide(firstOperand, secondOperand);
+            break;
+
+    }
+
+    if (result.toString().includes(".")) result = result.toFixed(2);
+    else result = result.toString();
+
+    updateDisplay(result);
+    secondOperand = operator = "";
+    firstOperand = result;
+    postOp = true;
+}
+
+const updateValue = (val) => {
+    if (!operator && postOp) {
+        firstOperand = "";
+        postOp = false;
+        decimalButton.disabled = false;
+    }
+
+    if (operator) {
+        secondOperand += val;
+        updateDisplay(secondOperand);
+        if (val === ".") decimalButton.disabled = true;
+    } else {
+        firstOperand += val;
+        updateDisplay(firstOperand);
+        if (val === ".") decimalButton.disabled = true;
+    }
+}
+
+const updateOperation = (val) => {
+    if (!firstOperand) return;
+
+    if (secondOperand && operator) {
+        operate();
+    }
+
+    operator = val;
+
+    if (decimalButton.disabled) decimalButton.disabled = false;
+}
+
+const updateDisplay = (val) => { display.textContent = val; }
+
+const evalInput = (val) => {
+    const operands = "1234567890.";
+    const operators = "/*-+";
+
+    if (val === 'C' || val === 'c') reset();
+    else if (val === '⌫' || val === 'Backspace') backspace();
+    else if (operators.includes(val)) updateOperation(val);
+    else if (val === "=") operate();
+    else if (operands.includes(val)) updateValue(val);
+}
+
+buttons.forEach(btn => {
+    if (btn.textContent === ".") decimalButton = btn;
+    btn.addEventListener('click', (e) => {
+        const val = e.target.textContent;
+        evalInput(val);
+    });
+});
+
+document.addEventListener('keydown', (e) => {
+    const val = e.key;
+    evalInput(val);
+});
